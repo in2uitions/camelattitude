73e78c4f562fb278f02f6aad5959c2e5ebea2e25
fadlallahjawad <79308991+in2uitions@users.noreply.github.com>
2022-03-04 11:48:36 +0200

    c2
    c2

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
-export const URL = "http://107.180.95.23:8159/"
+export const URL = "https://107.180.95.23:8159/"
 export const URL_FRONT = "https://localhost:3000/"
-export const image_url = "http://107.180.95.23:8159/assets/"
+export const image_url = "https://107.180.95.23:8159/assets/"
