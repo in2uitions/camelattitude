3e764ec28a7a9c55996b512f17ae037126c46ac0
fadlallahjawad <79308991+in2uitions@users.noreply.github.com>
2022-03-04 16:19:47 +0200

    a5
    a5

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
 export const URL = "https://camel.businessexchange.me/"
-export const URL_FRONT = "https://localhost:3000/"
+export const URL_FRONT = "https://camelattitude.vercel.app/"
 export const image_url = "https://camel.businessexchange.me/assets/"
